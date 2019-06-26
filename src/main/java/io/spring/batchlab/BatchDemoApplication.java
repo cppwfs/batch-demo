70d9c7259662e7c88670ea0a96d2b3d3fc6f2e50
Glenn Renfro <grenfro@pivotal.io>
2019-06-26 17:31:55 -0400

    Updated

--- src/main/java/io/spring/batchlab/BatchDemoApplication.java
+++ src/main/java/io/spring/batchlab/BatchDemoApplication.java
@@ -9,4 +9,5 @@ public class BatchDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BatchDemoApplication.class, args);
 	}
+
 }
--- src/main/java/io/spring/batchlab/configuration/BatchDemoConfiguration.java
+++ src/main/java/io/spring/batchlab/configuration/BatchDemoConfiguration.java
@@ -16,16 +16,23 @@
 
 package io.spring.batchlab.configuration;
 
+import io.spring.batchlab.BatchLabProperties;
+import javax.sql.DataSource;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.BatchConfigurer;
+import org.springframework.batch.core.configuration.annotation.DefaultBatchConfigurer;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.SimpleJobBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.launch.support.SimpleJobLauncher;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
@@ -33,80 +40,41 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.task.configuration.EnableTask;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ConcurrentTaskExecutor;
 
 @EnableBatchProcessing
-@EnableTask
 @Configuration
+@EnableTask
 public class BatchDemoConfiguration {
+
+	@Autowired
+	BatchLabProperties batchLabProperties;
 
 	private static final Log logger = LogFactory.getLog(BatchDemoConfiguration.class);
 
 
-	/**
-	 * The name of our job: {@value}.
-	 */
-	public static final String JOB_NAME = "batchDemoJob";
-
-	/**
-	 * The name of our step: {@value}. A job can have many steps, but this job has
-	 * only one.
-	 */
-	public static final String STEP_NAME = "step1";
-
-	// Created automatically by Spring Boot
+	@Autowired
 	public JobBuilderFactory jobBuilderFactory;
 
-	// Created automatically by Spring Boot
+	@Autowired
 	public StepBuilderFactory stepBuilderFactory;
 
-	/**
-	 * Get Spring to pass in the factories created by Spring Boot.
-	 *
-	 * @param jobBuilderFactory
-	 *            A builder for creating a new Job.
-	 * @param stepBuilderFactory
-	 *            A builder for creating a new Job Step.
-	 */
-	@Autowired
-	public BatchDemoConfiguration(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory) {
-		super();
-		this.jobBuilderFactory = jobBuilderFactory;
-		this.stepBuilderFactory = stepBuilderFactory;
-	}
-
-	/**
-	 * Setup the Job using the specified step. A job may
-	 * have many steps, this one has just one.
-	 *
-	 * @param step1
-	 *            The only step in this job.
-	 * @return
-	 */
 	@Bean
-	public Job importUserJob(Step step1) {
-		return jobBuilderFactory.get(JOB_NAME) // Get a builder for this job
-				.flow(step1) // The step that the job actually runs
-				.end() // End of job definition
-				.build(); // Build the job
+	public Job job1() {
+		SimpleJobBuilder jobBuilder =  jobBuilderFactory.get(batchLabProperties.getBatchName())
+				.start(stepBuilderFactory.get("job1stepX")
+						.tasklet(new Tasklet() {
+							@Override
+							public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+								logger.info("Job1 is sleeping");
+								Thread.sleep(30000);
+								logger.info("Job1 was finishing");
+								return RepeatStatus.FINISHED;
+							}
+						})
+						.build()).incrementer(new RunIdIncrementer());
+
+					return jobBuilder.build();
 	}
 
-	/**
-	 * Our Step just prints "Job was run" to the console.
-	 * @return The step.
-	 */
-	@Bean
-	public Step step1() {
-		return stepBuilderFactory.get(STEP_NAME) // Get a builder for this step
-				.tasklet(new DemoTasklet()).build();
-	}
-
-
-	public static class DemoTasklet implements Tasklet {
-		@Override
-		public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-			logger.info("Job was run");
-			//return RepeatStatus.FINISHED;
-			throw new IllegalStateException("No batch task for you!");
-		}
-	}
 }
